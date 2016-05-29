71d693a574b5bcaf5fe3d0516add2e149a4f861a
Krzysztof Wojtas <kwojtas@gmail.com>
2016-05-29 23:45:47 +0200

    2.1-2

--- chapter02.py
+++ chapter02.py
@@ -9,3 +9,13 @@ def insertion_sort(A):
             A[i + 1] = A[i]
             i -= 1
         A[i + 1] = key
+
+
+def nonincreasing_insertion_sort(A):
+    for j in irange(2, A.length):
+        key = A[j]
+        i = j - 1
+        while i > 0 and A[i] < key:
+            A[i + 1] = A[i]
+            i -= 1
+        A[i + 1] = key
--- tests/test_chapter02.py
+++ tests/test_chapter02.py
@@ -1,16 +1,26 @@
 import unittest
 
-from chapter02 import insertion_sort
+from chapter02 import *
 from data_structures import Array
 
 
 class InsertionSortTest(unittest.TestCase):
-    def test_sort_singleton(self):
+    def test_insertion_sort_singleton(self):
         array = Array([3])
         insertion_sort(array)
         self.assertEqual([3], array.data)
 
-    def test_sort_array(self):
+    def test_insertion_sort_array(self):
         array = Array([6, 1, 4, 2, 6, 7, 2])
         insertion_sort(array)
         self.assertEqual([1, 2, 2, 4, 6, 6, 7], array.data)
+
+    def test_nonincreasing_insertion_sort_singleton(self):
+        array = Array([3])
+        nonincreasing_insertion_sort(array)
+        self.assertEqual([3], array.data)
+
+    def test_nonincreasing_insertion_sort_array(self):
+        array = Array([6, 1, 4, 2, 6, 7, 2])
+        nonincreasing_insertion_sort(array)
+        self.assertEqual([7, 6, 6, 4, 2, 2, 1], array.data)
