ddbe4a40abcf9c39a176ab2e5b01d60fce008a5c
Krzysztof Wojtas <kwojtas@gmail.com>
2017-07-03 19:04:24 +0200

    6.2-5

---
+++ src/chapter06/ex6_2_5.py
@@ -0,0 +1,17 @@
+from chapter06.textbook import left, right
+
+
+def iterative_max_heapify(A, i):
+    while True:
+        l = left(i)
+        r = right(i)
+        if l <= A.heap_size and A[l] > A[i]:
+            largest = l
+        else:
+            largest = i
+        if r <= A.heap_size and A[r] > A[largest]:
+            largest = r
+        if largest == i:
+            return
+        A[i], A[largest] = A[largest], A[i]
+        i = largest
---
+++ test/chapter06/test_ex6_2_5.py
@@ -0,0 +1,12 @@
+import unittest
+
+from chapter06.ex6_2_5 import iterative_max_heapify
+from datastructures.heap import Heap
+
+
+class IterativeMaxHeapifyTest(unittest.TestCase):
+    def test_min_heapify(self):
+        heap = Heap([16, 4, 10, 14, 7, 9, 3, 2, 8, 1])
+        iterative_max_heapify(heap, 2)
+        self.assertEqual([16, 14, 10, 8, 7, 9, 3, 2, 4, 1], heap.data)
+        self.assertEqual(10,  heap.heap_size)
