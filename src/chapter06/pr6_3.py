91e8481daf3a6f78ac6c09378762ca47c1f3bde5
Krzysztof Wojtas <kwojtas@gmail.com>
2017-07-05 17:07:55 +0200

    6-3

---
+++ src/chapter06/pr6_3.py
@@ -0,0 +1,62 @@
+import math
+
+from datastructures.array import Array
+from util import scope
+
+
+def young_extract_min(Y, m, n, i, j):
+    if (i, j) == (m, n):
+        min = Y[i][j]
+        Y[i][j] = math.inf
+        return min
+    i_, j_ = i, j + 1
+    if i < m:
+        if j == n or Y[i + 1][j] < Y[i][j + 1]:
+            i_, j_ = i + 1, j
+    Y[i][j], Y[i_][j_] = Y[i_][j_], Y[i][j]
+    return young_extract_min(Y, m, n, i_, j_)
+
+
+def youngify(Y, i, j):
+    i_, j_ = i, j
+    if i > 1 and Y[i - 1][j] > Y[i_][j_]:
+        i_, j_ = i - 1, j
+    if j > 1 and Y[i][j - 1] > Y[i_][j_]:
+        i_, j_ = i, j - 1
+    if (i_, j_) != (i, j):
+        Y[i][j], Y[i_][j_] = Y[i_][j_], Y[i][j]
+        youngify(Y, i_, j_)
+
+
+def young_insert(Y, m, n, key):
+    Y[m][n] = key
+    youngify(Y, m, n)
+
+
+def young_sort(A):
+    n = int(math.sqrt(A.length))
+    Y = _init_young(n)
+    for i in scope(1, n ** 2):
+        young_insert(Y, n, n, A[i])
+    for i in scope(1, n ** 2):
+        A[i] = young_extract_min(Y, n, n, 1, 1)
+
+
+def _init_young(n):
+    Y = Array.of_length(n)
+    for i in scope(1, n):
+        Y[i] = Array([math.inf] * n)
+    return Y
+
+
+def young_search(Y, m, n, v):
+    i = 1
+    j = n
+    while i <= m and j >= 1:
+        if v == Y[i][j]:
+            return True
+        if v > Y[i][j]:
+            i = i + 1
+        else:
+            j = j - 1
+    return False
---
+++ test/chapter06/test_pr6_3.py
@@ -0,0 +1,79 @@
+import unittest
+
+from chapter06.pr6_3 import *
+from datastructures.array import Array
+
+
+class YoungTest(unittest.TestCase):
+    def test_young_extract_min(self):
+        young = Array([
+            Array([2, 3, 14, 16]),
+            Array([4, 8, math.inf, math.inf]),
+            Array([5, 12, math.inf, math.inf]),
+            Array([9, math.inf, math.inf, math.inf])
+        ])
+        min = young_extract_min(young, 4, 4, 1, 1)
+        expected_young = Array([
+            Array([3, 8, 14, 16]),
+            Array([4, 12, math.inf, math.inf]),
+            Array([5, math.inf, math.inf, math.inf]),
+            Array([9, math.inf, math.inf, math.inf])
+        ])
+        self.assertEqual(2, min)
+        self.assertEqual(expected_young, young)
+
+    def test_youngify(self):
+        young = Array([
+            Array([2, 3, 14, 16]),
+            Array([4, 8, math.inf, math.inf]),
+            Array([5, 1, math.inf, math.inf]),
+            Array([9, math.inf, math.inf, math.inf])
+        ])
+        youngify(young, 3, 2)
+        expected_young = Array([
+            Array([1, 3, 14, 16]),
+            Array([2, 4, math.inf, math.inf]),
+            Array([5, 8, math.inf, math.inf]),
+            Array([9, math.inf, math.inf, math.inf])
+        ])
+        self.assertEqual(expected_young, young)
+
+    def test_young_insert(self):
+        young = Array([
+            Array([2, 3, 14, 16]),
+            Array([4, 8, math.inf, math.inf]),
+            Array([5, 12, math.inf, math.inf]),
+            Array([9, math.inf, math.inf, math.inf])
+        ])
+        young_insert(young, 4, 4, 7)
+        expected_young = Array([
+            Array([2, 3, 7, 16]),
+            Array([4, 8, 14, math.inf]),
+            Array([5, 12, math.inf, math.inf]),
+            Array([9, math.inf, math.inf, math.inf])
+        ])
+        self.assertEqual(expected_young, young)
+
+    def test_young_sort(self):
+        data = [5, 7, 9, 2, 6, 8, 6, 6, 3, 1, 7, 8, 6, 3, 7, 8]
+        array = Array(data)
+        young_sort(array)
+        self.assertEqual(sorted(data), array.data)
+
+    def test_young_search_positive(self):
+        young = Array([
+            Array([2, 3, 14, 16]),
+            Array([4, 8, math.inf, math.inf]),
+            Array([5, 12, math.inf, math.inf]),
+            Array([9, math.inf, math.inf, math.inf])
+        ])
+        self.assertTrue(young_search(young, 4, 4, 5))
+
+    def test_young_search_negative(self):
+        young = Array([
+            Array([2, 3, 14, 16]),
+            Array([4, 8, math.inf, math.inf]),
+            Array([5, 12, math.inf, math.inf]),
+            Array([9, math.inf, math.inf, math.inf])
+        ])
+        self.assertFalse(young_search(young, 4, 4, 7))
