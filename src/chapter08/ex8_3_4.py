0e7c9446c191af56099ce0a0d43e54dd0a8d0e7a
Krzysztof Wojtas <kwojtas@gmail.com>
2017-07-07 02:47:44 +0200

    8.3-4

---
+++ src/chapter08/ex8_3_4.py
@@ -0,0 +1,13 @@
+def below_square_sort(A):
+    n = A.length
+    _stable_sort_on_digit(A, 1, n)
+    _stable_sort_on_digit(A, 2, n)
+
+
+def _stable_sort_on_digit(A, digit, base):
+    A.data.sort(key=lambda x: _get_digit(x, digit, base))
+
+
+def _get_digit(number, digit, base):
+    power_of_base = base ** (digit - 1)
+    return number // power_of_base % base
