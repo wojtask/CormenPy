47c905c1f0ae36d30163a1104fcd50e1d27d4f98
Krzysztof Wojtas <kwojtas@gmail.com>
2017-07-07 22:22:21 +0200

    textbook chapter 9

---
+++ src/chapter09/textbook.py
@@ -0,0 +1,84 @@
+import math
+
+from chapter02.textbook import insertion_sort
+from chapter07.textbook import randomized_partition
+from datastructures.array import Array
+from util import between
+
+
+def minimum(A):
+    min = A[1]
+    for i in between(2, A.length):
+        if min > A[i]:
+            min = A[i]
+    return min
+
+
+def minimum_maximum(A):
+    n = A.length
+    if n % 2 == 0:
+        min = math.inf
+        max = -math.inf
+        i = 1
+    else:
+        min = max = A[1]
+        i = 2
+    while i < n:
+        if A[i] < A[i + 1]:
+            if A[i] < min:
+                min = A[i]
+            if A[i + 1] > max:
+                max = A[i + 1]
+        else:
+            if A[i + 1] < min:
+                min = A[i + 1]
+            if A[i] > max:
+                max = A[i]
+        i += 2
+    return min, max
+
+
+def randomized_select(A, p, r, i):
+    if p == r:
+        return A[p]
+    q = randomized_partition(A, p, r)
+    k = q - p + 1
+    if i == k:
+        return A[q]
+    elif i < k:
+        return randomized_select(A, p, q - 1, i)
+    else:
+        return randomized_select(A, q + 1, r, i - k)
+
+
+def select(A, p, r, i):
+    n = r - p + 1
+    if n == 1:
+        return A[p]
+    fives = [Array(A.data[k:k + 5]) for k in range(0, n, 5)]
+    for group in fives:
+        insertion_sort(group)
+    medians = Array([group[(group.length + 1) // 2] for group in fives])
+    x = select(medians, 1, medians.length, (medians.length + 1) // 2)
+    q = _partition_around(A, p, r, x)
+    k = q - p + 1
+    if i == k:
+        return x
+    elif i < k:
+        return select(A, p, q - 1, i)
+    else:
+        return select(A, q + 1, r, i - k)
+
+
+def _partition_around(A, p, r, x):
+    q = p
+    while A[q] != x:
+        q += 1
+    A[q], A[r] = A[r], A[q]
+    i = p - 1
+    for j in between(p, r - 1):
+        if A[j] < x:
+            i = i + 1
+            A[i], A[j] = A[j], A[i]
+    A[i + 1], A[r] = A[r], A[i + 1]
+    return i + 1
---
+++ test/chapter09/test_textbook.py
@@ -0,0 +1,38 @@
+import unittest
+
+from chapter09.textbook import *
+from datastructures.array import Array
+
+
+class Chapter09Test(unittest.TestCase):
+    def test_minimum(self):
+        data = [5, 7, 9, 2, 6, 8, 6, 6, 3, 1, 7, 8]
+        array = Array(data)
+        min = minimum(array)
+        self.assertEqual(1, min)
+
+    def test_minimum_maximum_even_size(self):
+        data = [5, 7, 9, 2, 6, 8, 6, 6, 3, 1, 7, 8]
+        array = Array(data)
+        min, max = minimum_maximum(array)
+        self.assertEqual(1, min)
+        self.assertEqual(9, max)
+
+    def test_minimum_maximum_odd_size(self):
+        data = [3, 5, 2, 6, 6, 5, 1, 8, 9, 4, 7, 4, 2]
+        array = Array(data)
+        min, max = minimum_maximum(array)
+        self.assertEqual(1, min)
+        self.assertEqual(9, max)
+
+    def test_randomized_select(self):
+        data = [5, 7, 9, 2, 6, 8, 6, 6, 3, 1, 7, 8]
+        array = Array(data)
+        x = randomized_select(array, 1, array.length, 5)
+        self.assertEqual(6, x)
+
+    def test_select(self):
+        data = [5, 9, 7, 9, 2, 8, 10, 6, 8, 6, 15, 12, 6, 3, 1, 7, 7, 8]
+        array = Array(data)
+        x = select(array, 1, array.length, 5)
+        self.assertEqual(6, x)
