3fb250740ae886b737b44a4e27bb604b8b71e861
Krzysztof Wojtas <kwojtas@gmail.com>
2017-07-07 18:24:06 +0200

    8-2

---
+++ src/chapter08/pr8_2.py
@@ -0,0 +1,33 @@
+from datastructures.standard_array import StandardArray
+from util import between
+
+
+def bitwise_sort(A):
+    n = A.length
+    i = 1
+    j = n
+    while i < j:
+        A[i], A[j] = A[j], A[i]
+        while i <= n and A[i] == 0:
+            i = i + 1
+        while j >= 1 and A[j] == 1:
+            j = j - 1
+
+
+def counting_sort_in_place(A, k):
+    C = StandardArray.of_length(k + 1)
+    for i in between(0, k):
+        C[i] = 0
+    for j in between(1, A.length):
+        C[A[j]] = C[A[j]] + 1
+    for i in between(1, k):
+        C[i] = C[i] + C[i - 1]
+    C_ = StandardArray(C.data)
+    i = 1
+    while i <= A.length - 1:
+        key = A[i]
+        if C_[key - 1] < i <= C_[key]:
+            i = i + 1
+        else:
+            A[i], A[C[key]] = A[C[key]], A[i]
+            C[key] = C[key] - 1
---
+++ test/chapter08/test_pr8_2.py
@@ -0,0 +1,20 @@
+import unittest
+
+from chapter08.pr8_2 import bitwise_sort, counting_sort_in_place
+from datastructures.array import Array
+
+
+class Problem8_2Test(unittest.TestCase):
+    def test_bitwise_sort(self):
+        data = [1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0]
+        array = Array(data)
+        bitwise_sort(array)
+        expected_array = Array(sorted(data))
+        self.assertEqual(expected_array, array)
+
+    def test_counting_sort_in_place(self):
+        data = [7, 1, 3, 1, 2, 4, 5, 7, 2, 4, 3]
+        array = Array(data)
+        counting_sort_in_place(array, 7)
+        expected_array = Array(sorted(data))
+        self.assertEqual(expected_array, array)
