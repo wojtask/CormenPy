94eaccfdd8c25ad1222263d32f78ab07f420b4b3
Krzysztof Wojtas <kwojtas@gmail.com>
2017-07-10 01:36:07 +0200

    tests for BinaryTree

--- test/test_chapter12/test_textbook.py
+++ test/test_chapter12/test_textbook.py
@@ -1,22 +1,23 @@
-import unittest
-
 import io
+import unittest
 from contextlib import redirect_stdout
 
 from chapter12.textbook import *
 from datastructures.array import Array
 from datastructures.binary_tree import BinaryTree, Node
+from test.test_datastructures.test_binary_tree import BinaryTreeTest, binary_tree_to_list
 
 
 class Chapter12Test(unittest.TestCase):
     def setUp(self):
-        self.tree = BinaryTree(Node(10, data='ten',
-                               left=Node(4, data='four',
-                                         left=Node(1, data='one')),
-                               right=Node(14, data='fourteen',
-                                          left=Node(11, data='eleven'),
-                                          right=Node(19, data='nineteen',
-                                                     right=Node(20, data='twenty')))))
+        self.tree = BinaryTree(Node(10,
+                               left=Node(4,
+                                         left=Node(1)),
+                               right=Node(14,
+                                          left=Node(11),
+                                          right=Node(19,
+                                                     right=Node(20)))))
+        self.btt = BinaryTreeTest()
 
     def test_inorder_tree_walk_empty_tree(self):
         captured_output = io.StringIO()
@@ -33,7 +34,6 @@ class Chapter12Test(unittest.TestCase):
     def test_tree_search_positive(self):
         x = tree_search(self.tree.root, 11)
         self.assertEqual(11, x.key)
-        self.assertEqual('eleven', x.data)
 
     def test_tree_search_negative(self):
         x = tree_search(self.tree.root, 17)
@@ -42,7 +42,6 @@ class Chapter12Test(unittest.TestCase):
     def test_iterative_tree_search_positive(self):
         x = iterative_tree_search(self.tree.root, 11)
         self.assertEqual(11, x.key)
-        self.assertEqual('eleven', x.data)
 
     def test_iterative_tree_search_negative(self):
         x = iterative_tree_search(self.tree.root, 17)
@@ -71,59 +70,42 @@ class Chapter12Test(unittest.TestCase):
             inorder_tree_walk_(self.tree)
         self.assertEqual(['1', '4', '10', '11', '14', '19', '20'], captured_output.getvalue().splitlines())
 
-    def binary_tree_to_list(self, node):
-        if node is None:
-            return []
-        return [node.key] + self.binary_tree_to_list(node.left) + self.binary_tree_to_list(node.right)
-
-    def assert_binary_search_tree(self, x):
-        if x.left is not None:
-            left_keys = self.binary_tree_to_list(x.left)
-            for key in left_keys:
-                self.assertTrue(key <= x.key)
-            self.assert_binary_search_tree(x.left)
-        if x.right is not None:
-            right_keys = self.binary_tree_to_list(x.right)
-            for key in right_keys:
-                self.assertTrue(key >= x.key)
-            self.assert_binary_search_tree(x.right)
-
     def test_tree_insert_to_empty_tree(self):
         tree = BinaryTree()
         tree_insert(tree, Node(12))
-        keys = self.binary_tree_to_list(tree.root)
+        keys = binary_tree_to_list(tree.root)
         self.assertEqual([12], keys)
-        self.binary_tree_to_list(tree.root)
+        self.btt.assert_binary_search_tree(tree.root)
 
     def test_tree_insert(self):
         tree_insert(self.tree, Node(12))
-        keys = self.binary_tree_to_list(self.tree.root)
+        keys = binary_tree_to_list(self.tree.root)
         self.assertEqual([1, 4, 10, 11, 12, 14, 19, 20], sorted(keys))
-        self.assert_binary_search_tree(self.tree.root)
+        self.btt.assert_binary_search_tree(self.tree.root)
 
     def test_tree_delete_leaf(self):
         node = self.tree.root.right.left  # a leaf
         y = tree_delete(self.tree, node)
         self.assertEqual(11, y.key)
-        keys = self.binary_tree_to_list(self.tree.root)
+        keys = binary_tree_to_list(self.tree.root)
         self.assertEqual([1, 4, 10, 14, 19, 20], sorted(keys))
-        self.assert_binary_search_tree(self.tree.root)
+        self.btt.assert_binary_search_tree(self.tree.root)
 
     def test_tree_delete_node_with_one_child(self):
         node = self.tree.root.left  # a node with one child
         y = tree_delete(self.tree, node)
         self.assertEqual(4, y.key)
-        keys = self.binary_tree_to_list(self.tree.root)
+        keys = binary_tree_to_list(self.tree.root)
         self.assertEqual([1, 10, 11, 14, 19, 20], sorted(keys))
-        self.assert_binary_search_tree(self.tree.root)
+        self.btt.assert_binary_search_tree(self.tree.root)
 
     def test_tree_delete_node_with_two_children(self):
         node = self.tree.root.right  # a node with two children (successor's key = 19)
         y = tree_delete(self.tree, node)
         self.assertEqual(19, y.key)
-        keys = self.binary_tree_to_list(self.tree.root)
+        keys = binary_tree_to_list(self.tree.root)
         self.assertEqual([1, 4, 10, 11, 19, 20], sorted(keys))
-        self.assert_binary_search_tree(self.tree.root)
+        self.btt.assert_binary_search_tree(self.tree.root)
 
     def test_tree_delete_from_single_node_tree(self):
         tree = BinaryTree(Node(10))
---
+++ test/test_datastructures/test_binary_tree.py
@@ -0,0 +1,39 @@
+import unittest
+
+from datastructures.binary_tree import BinaryTree, Node
+
+
+class BinaryTreeTest(unittest.TestCase):
+    def test_create_empty_binary_tree(self):
+        tree = BinaryTree()
+        self.assertIsNone(tree.root)
+
+    def test_create_binary_tree(self):
+        left = Node(3)
+        right = Node(20)
+        root = Node(17, left=left, right=right)
+        tree = BinaryTree(root)
+        self.assertEqual(tree.root, root)
+        self.assertIsNone(root.p)
+        self.assertEqual(left, root.left)
+        self.assertEqual(root, left.p)
+        self.assertEqual(right, root.right)
+        self.assertEqual(root, right.p)
+
+    def assert_binary_search_tree(self, x):
+        if x.left is not None:
+            left_keys = binary_tree_to_list(x.left)
+            for key in left_keys:
+                self.assertTrue(key <= x.key)
+            self.assert_binary_search_tree(x.left)
+        if x.right is not None:
+            right_keys = binary_tree_to_list(x.right)
+            for key in right_keys:
+                self.assertTrue(key >= x.key)
+            self.assert_binary_search_tree(x.right)
+
+
+def binary_tree_to_list(node):
+    if node is None:
+        return []
+    return [node.key] + binary_tree_to_list(node.left) + binary_tree_to_list(node.right)
