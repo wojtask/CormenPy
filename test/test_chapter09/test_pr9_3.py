1f04db8119a39a3edce0c20c2ed7779c3fb2b335
Krzysztof Wojtas <kwojtas@gmail.com>
2017-07-10 12:41:49 +0200

    9-3

---
+++ test/test_chapter09/test_pr9_3.py
@@ -0,0 +1,12 @@
+import unittest
+
+from chapter09.pr9_3 import small_order_select
+from datastructures.array import Array
+
+
+class Problem9_3Test(unittest.TestCase):
+    def test_small_order_select(self):
+        data = [5, 12, 1, 0, 13, 12, 0, 10, 9, 1, 4, 3, 16, 15, 19, 6, 11, 20, 2]
+        array = Array(data)
+        x = small_order_select(array, 3)
+        self.assertEqual(1, x)
