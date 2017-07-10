7e0008b3f49b1f2e57aa2024825c0923d331c4ac
Krzysztof Wojtas <kwojtas@gmail.com>
2017-07-10 12:28:05 +0200

    12-3

---
+++ src/chapter12/pr12_3.py
@@ -0,0 +1,19 @@
+from util import between
+
+
+def randomly_built_tree_quicksort(A, p, r):
+    if p < r:
+        q = _randomly_built_tree_partition(A, p, r)
+        randomly_built_tree_quicksort(A, p, q - 1)
+        randomly_built_tree_quicksort(A, q + 1, r)
+
+
+def _randomly_built_tree_partition(A, p, r):
+    x = A[p]
+    i = p
+    for j in between(p + 1, r):
+        if A[j] <= x:
+            i = i + 1
+            A[i], A[j] = A[j], A[i]
+    A[i], A[p] = A[p], A[i]
+    return i
---
+++ test/test_chapter12/test_pr12_3.py
@@ -0,0 +1,13 @@
+import unittest
+
+from chapter12.pr12_3 import randomly_built_tree_quicksort
+from datastructures.array import Array
+
+
+class Problem12_3Test(unittest.TestCase):
+    def test_bit_strings_sort(self):
+        data = [5, 7, 9, 2, 6, 8, 6, 6, 3, 1, 7, 8]
+        array = Array(data)
+        randomly_built_tree_quicksort(array, 1, array.length)
+        expected_array = Array(sorted(data))
+        self.assertEqual(expected_array, array)
