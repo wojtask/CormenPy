c78a7715c99357d7e527990ab8b6bf5257c68bea
Krzysztof Wojtas <kwojtas@gmail.com>
2017-07-11 14:36:02 +0200

    adding ability to compare BinaryTrees

--- src/datastructures/binary_tree.py
+++ src/datastructures/binary_tree.py
@@ -2,6 +2,19 @@ class BinaryTree:
     def __init__(self, root=None):
         self.root = root
 
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, self.__class__):
+            return not self.__eq__(other)
+        return NotImplemented
+
+    def __hash__(self):
+        return hash(tuple(sorted(self.__dict__.items())))
+
 
 class Node:
     def __init__(self, key, data=None, left=None, right=None):
@@ -14,3 +27,17 @@ class Node:
         if right is not None:
             right.p = self
         self.p = None
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.key == other.key and self.data == other.data and \
+                   self.left == other.left and self.right == other.right
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, self.__class__):
+            return not self.__eq__(other)
+        return NotImplemented
+
+    def __hash__(self):
+        return hash(tuple(sorted(self.__dict__.items())))
