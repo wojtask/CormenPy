93d0ed18e7f958db6ff8ebc67ff29f692450f7ff
Krzysztof Wojtas <kwojtas@gmail.com>
2017-07-13 22:20:14 +0200

    9.3-3 -- fix in best_case_quicksort

--- src/chapter09/ex9_3_3.py
+++ src/chapter09/ex9_3_3.py
@@ -3,7 +3,8 @@ from chapter09.textbook import select
 
 def best_case_quicksort(A, p, r):
     if p < r:
+        n = r - p + 1
+        select(A, p, r, (n + 1) // 2)
         q = (p + r) // 2
-        select(A, p, r, q)
         best_case_quicksort(A, p, q - 1)
         best_case_quicksort(A, q + 1, r)
