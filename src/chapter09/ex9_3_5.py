3a5974d2d28c4276afbfb38fe1873ae3b2b84d2f
Krzysztof Wojtas <kwojtas@gmail.com>
2017-07-14 01:10:59 +0200

    9.3-5 -- fix in randomized_blackbox_select

--- src/chapter09/ex9_3_5.py
+++ src/chapter09/ex9_3_5.py
@@ -1,12 +1,17 @@
-from chapter09.textbook import select, partition_around
+from chapter09.textbook import partition_around, select
+
+
+def _get_median_blackbox(A, p, r):
+    n = r - p + 1
+    return select(A, p, r, (n + 1) // 2)
 
 
 def randomized_blackbox_select(A, p, r, i):
     if p == r:
         return A[p]
-    q = (p + r) // 2
-    x = select(A, p, r, q)
+    x = _get_median_blackbox(A, p, r)
     partition_around(A, p, r, x)
+    q = (p + r) // 2
     k = q - p + 1
     if i == k:
         return A[q]
