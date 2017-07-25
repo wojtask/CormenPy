3759279174888866617aa90681f4220f408dbf4e
Krzysztof Wojtas <kwojtas@gmail.com>
2017-07-26 00:48:29 +0200

    10.1-2

---
+++ src/chapter10/ex10_1_2.py
@@ -0,0 +1,38 @@
+def left_stack_push(A, x):
+    if _stacks_full(A):
+        raise RuntimeError('overflow')
+    A.left_top += 1
+    A[A.left_top] = x
+
+
+def _stacks_full(A):
+    return A.left_top == A.right_top - 1
+
+
+def left_stack_pop(A):
+    if _left_stack_empty(A):
+        raise RuntimeError('underflow')
+    A.left_top -= 1
+    return A[A.left_top + 1]
+
+
+def _left_stack_empty(A):
+    return A.left_top == 0
+
+
+def right_stack_push(A, x):
+    if _stacks_full(A):
+        raise RuntimeError('overflow')
+    A.right_top -= 1
+    A[A.right_top] = x
+
+
+def right_stack_pop(A):
+    if _right_stack_empty(A):
+        raise RuntimeError('underflow')
+    A.right_top += 1
+    return A[A.right_top - 1]
+
+
+def _right_stack_empty(A):
+    return A.right_top == A.length + 1
--- test/test_solutions10.py
+++ test/test_solutions10.py
@@ -3,11 +3,74 @@ from unittest import TestCase
 
 from hamcrest import *
 
+from chapter10.ex10_1_2 import left_stack_push, left_stack_pop, right_stack_push, right_stack_pop
 from chapter10.ex10_1_4 import queue_empty, enqueue_, dequeue_
 from test_datastructures.array_util import random_int_array
 
 
 class Solutions10Test(TestCase):
+
+    def test_left_stack_push(self):
+        size = 10
+        array, data = random_int_array(min_size=size, max_size=size)
+        left_top_before_insert = array.left_top = random.randint(0, size)
+        right_top_before_insert = array.right_top = random.randint(array.left_top + 1, size + 1)
+        x = random.randint(0, 999)
+
+        if array.left_top == array.right_top - 1:
+            assert_that(calling(left_stack_push).with_args(array, x), raises(RuntimeError, 'overflow'))
+        else:
+            left_stack_push(array, x)
+
+            assert_that(array.left_top, is_(equal_to(left_top_before_insert + 1)))
+            assert_that(array.right_top, is_(equal_to(right_top_before_insert)))
+            assert_that(array[array.left_top], is_(equal_to(x)))
+
+    def test_left_stack_pop(self):
+        size = 10
+        array, data = random_int_array(min_size=size, max_size=size)
+        left_top_before_insert = array.left_top = random.randint(0, size)
+        right_top_before_insert = array.right_top = random.randint(array.left_top + 1, size + 1)
+
+        if array.left_top == 0:
+            assert_that(calling(left_stack_pop).with_args(array), raises(RuntimeError, 'underflow'))
+        else:
+            actual_deleted = left_stack_pop(array)
+
+            assert_that(array.left_top, is_(equal_to(left_top_before_insert - 1)))
+            assert_that(array.right_top, is_(equal_to(right_top_before_insert)))
+            assert_that(actual_deleted, is_(equal_to(array[left_top_before_insert])))
+
+    def test_right_stack_push(self):
+        size = 10
+        array, data = random_int_array(min_size=size, max_size=size)
+        left_top_before_insert = array.left_top = random.randint(0, size)
+        right_top_before_insert = array.right_top = random.randint(array.left_top + 1, size + 1)
+        x = random.randint(0, 999)
+
+        if array.left_top == array.right_top - 1:
+            assert_that(calling(right_stack_push).with_args(array, x), raises(RuntimeError, 'overflow'))
+        else:
+            right_stack_push(array, x)
+
+            assert_that(array.left_top, is_(equal_to(left_top_before_insert)))
+            assert_that(array.right_top, is_(equal_to(right_top_before_insert - 1)))
+            assert_that(array[array.right_top], is_(equal_to(x)))
+
+    def test_right_stack_pop(self):
+        size = 10
+        array, data = random_int_array(min_size=size, max_size=size)
+        left_top_before_insert = array.left_top = random.randint(0, size)
+        right_top_before_insert = array.right_top = random.randint(array.left_top + 1, size + 1)
+
+        if array.right_top == array.length + 1:
+            assert_that(calling(right_stack_pop).with_args(array), raises(RuntimeError, 'underflow'))
+        else:
+            actual_deleted = right_stack_pop(array)
+
+            assert_that(array.left_top, is_(equal_to(left_top_before_insert)))
+            assert_that(array.right_top, is_(equal_to(right_top_before_insert + 1)))
+            assert_that(actual_deleted, is_(equal_to(array[right_top_before_insert])))
 
     def test_queue_empty(self):
         size = 5
