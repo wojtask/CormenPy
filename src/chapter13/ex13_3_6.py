206ea3c40d7e492a5c710ded5c97c4fd29d0d529
Krzysztof Wojtas <kwojtas@gmail.com>
2017-08-07 11:30:27 +0200

    13.3-6

---
+++ src/chapter13/ex13_3_6.py
@@ -0,0 +1,103 @@
+from chapter10.textbook import push, pop
+from datastructures.array import Array
+from datastructures.red_black_tree import Red, Black
+
+
+def rb_parentless_insert(T, z):
+    y = T.nil
+    x = T.root
+    path_length = _get_path_length_from_root_to_leaf(T, z)
+    S = Array.of_length(path_length + 1)
+    S.top = 0
+    push(S, T.nil)
+    while x is not T.nil:
+        y = x
+        push(S, y)
+        if z.key < x.key:
+            x = x.left
+        else:
+            x = x.right
+    if y is T.nil:
+        T.root = z
+    else:
+        if z.key < y.key:
+            y.left = z
+        else:
+            y.right = z
+    z.left = T.nil
+    z.right = T.nil
+    z.color = Red
+    rb_parentless_insert_fixup(T, S, z)
+
+
+def _get_path_length_from_root_to_leaf(T, z):
+    path_length = 0
+    x = T.root
+    while x is not T.nil:
+        path_length += 1
+        if z.key < x.key:
+            x = x.left
+        else:
+            x = x.right
+    return path_length
+
+
+def rb_parentless_insert_fixup(T, S, z):
+    p = pop(S)
+    while p.color == Red:
+        r = pop(S)
+        if p is r.left:
+            y = r.right
+            if y.color == Red:
+                y.color = p.color = Black
+                r.color = Red
+                z = r
+                p = pop(S)
+            else:
+                if z is p.right:
+                    z, p = p, z
+                    rb_parentless_left_rotate(T, z, r)
+                p.color = Black
+                r.color = Red
+                rb_parentless_right_rotate(T, r, pop(S))
+        else:
+            y = r.left
+            if y.color is Red:
+                y.color = p.color = Black
+                r.color = Red
+                z = r
+                p = pop(S)
+            else:
+                if z is p.left:
+                    z, p = p, z
+                    rb_parentless_right_rotate(T, z, r)
+                p.color = Black
+                r.color = Red
+                rb_parentless_left_rotate(T, r, pop(S))
+    T.root.color = Black
+
+
+def rb_parentless_left_rotate(T, x, p):
+    y = x.right
+    x.right = y.left
+    if p is T.nil:
+        T.root = y
+    else:
+        if x is p.left:
+            p.left = y
+        else:
+            p.right = y
+    y.left = x
+
+
+def rb_parentless_right_rotate(T, x, p):
+    y = x.left
+    x.left = y.right
+    if p is T.nil:
+        T.root = y
+    else:
+        if x is p.right:
+            p.right = y
+        else:
+            p.left = y
+    y.right = x
--- test/test_chapter13/test_solutions.py
+++ test/test_chapter13/test_solutions.py
@@ -3,18 +3,34 @@ from unittest import TestCase
 
 from hamcrest import *
 
+from chapter13.ex13_3_6 import rb_parentless_insert
 from chapter13.pr13_1 import persistent_tree_insert
 from chapter13.pr13_3 import avl_insert_wrapper
 from chapter13.pr13_4 import treap_insert
 from datastructures import avl_tree as avl, treap as tp
 from datastructures.avl_tree import AVLTree
 from datastructures.binary_tree import BinaryTree
+from datastructures.red_black_tree import RedBlackTree, Node
 from datastructures.treap import Treap
 from tree_util import assert_binary_search_tree, get_binary_tree_keys, assert_avl_tree, \
-    assert_parent_pointers_consistent, assert_treap
+    assert_parent_pointers_consistent, assert_treap, assert_red_black_tree
 
 
 class Solutions13Test(TestCase):
+
+    def test_rb_parentless_insert(self):
+        keys = [random.randrange(1000) for _ in range(20)]
+        tree = RedBlackTree()
+
+        for key in keys:
+
+            rb_parentless_insert(tree, Node(key))
+
+            assert_red_black_tree(tree)
+
+        actual_keys = get_binary_tree_keys(tree, sentinel=tree.nil)
+        assert_that(actual_keys, contains_inanyorder(*keys))
+
 
     def test_persistent_tree_insert(self):
         keys = [random.randrange(1000) for _ in range(20)]
