6b8ac2a1939d887ce53b44a16f0d335311122b93
Krzysztof Wojtas <kwojtas@gmail.com>
2017-11-07 17:57:01 +0100

    15.2 -- matrix_chain_order and print_optimal_parens

--- src/chapter15/textbook.py
+++ src/chapter15/textbook.py
@@ -1,3 +1,5 @@
+import math
+
 from datastructures.matrix import Matrix
 from util import between, rbetween
 
@@ -48,3 +50,31 @@ def matrix_multiply(A, B):
                 for k in between(1, A.columns):
                     C[i, j] = C[i, j] + A[i, k] * B[k, j]
         return C
+
+
+def matrix_chain_order(p):
+    n = p.length - 1
+    m = Matrix.of_dimensions(n, n)
+    s = Matrix.of_dimensions(n, n)
+    for i in between(1, n):
+        m[i, i] = 0
+    for l in between(2, n):
+        for i in between(1, n - l + 1):
+            j = i + l - 1
+            m[i, j] = math.inf
+            for k in between(i, j - 1):
+                q = m[i, k] + m[k + 1, j] + p[i - 1] * p[k] * p[j]
+                if q < m[i, j]:
+                    m[i, j] = q
+                    s[i, j] = k
+    return m, s
+
+
+def print_optimal_parens(s, i, j):
+    if i == j:
+        print('A' + str(i), end='')
+    else:
+        print('(', end='')
+        print_optimal_parens(s, i, s[i, j])
+        print_optimal_parens(s, s[i, j] + 1, j)
+        print(')', end='')
--- test/test_chapter15/test_textbook.py
+++ test/test_chapter15/test_textbook.py
@@ -1,4 +1,5 @@
 import io
+import math
 import random
 from contextlib import redirect_stdout
 from unittest import TestCase
@@ -7,8 +8,10 @@ import numpy
 from hamcrest import *
 
 from array_util import get_random_matrix, get_random_array
-from chapter15.textbook import fastest_way, print_stations, matrix_multiply
-from util import rbetween
+from chapter15.textbook import fastest_way, print_stations, matrix_multiply, matrix_chain_order, print_optimal_parens
+from datastructures.matrix import Matrix
+from datastructures.standard_array import StandardArray
+from util import rbetween, between
 
 
 def get_other_line(current_line):
@@ -42,8 +45,34 @@ def get_assembly_time_based_on_lines(lines, last_line, a, t, e, x, n):
     return assembly_time
 
 
-class Textbook15Test(TestCase):
+def get_minimum_matrix_product_cost(dimensions, i, j):
+    if i == j:
+        return 0
+    minimum_cost = math.inf
+    for k in between(i, j - 1):
+        cost = get_minimum_matrix_product_cost(dimensions, i, k) \
+               + get_minimum_matrix_product_cost(dimensions, k + 1, j) \
+               + dimensions[i - 1] * dimensions[k] * dimensions[j]
+        minimum_cost = min(cost, minimum_cost)
+    return minimum_cost
 
+
+def get_matrix_product_cost_based_on_solution(solution, dimensions, i, j):
+    if i == j:
+        return 0
+    k = solution[i, j]
+    return get_matrix_product_cost_based_on_solution(solution, dimensions, i, k) \
+           + get_matrix_product_cost_based_on_solution(solution, dimensions, k + 1, j) \
+           + dimensions[i - 1] * dimensions[k] * dimensions[j]
+
+
+def get_optimal_parens_brute_force(s, i, j):
+    if i == j:
+        return 'A' + str(i)
+    return '(' + get_optimal_parens_brute_force(s, i, s[i, j]) + get_optimal_parens_brute_force(s, s[i, j] + 1, j) + ')'
+
+
+class Textbook15Test(TestCase):
     def test_fastest_way(self):
         n = random.randint(1, 10)
         a, _ = get_random_matrix(2, n)
@@ -57,7 +86,6 @@ class Textbook15Test(TestCase):
         assert_that(actual_assembly_time, is_(equal_to(expected_assembly_time)))
         expected_assembly_time = get_assembly_time_based_on_lines(lines, last_line, a, t, e, x, n)
         assert_that(actual_assembly_time, is_(equal_to(expected_assembly_time)))
-
 
     def test_print_stations(self):
         n = random.randint(1, 10)
@@ -78,7 +106,6 @@ class Textbook15Test(TestCase):
             expected_output.append('line ' + str(i) + ', station ' + str(j - 1))
         assert_that(actual_output, is_(equal_to(expected_output)))
 
-
     def test_matrix_multiply(self):
         rows1 = random.randint(1, 3)
         columns1 = random.randint(1, 3)
@@ -96,3 +123,29 @@ class Textbook15Test(TestCase):
             assert_that(actual_product.columns, is_(equal_to(columns2)))
             expected_product = numpy.dot(elements1, elements2)
             assert_that(actual_product.elements, is_(equal_to(expected_product.tolist())))
+
+    def test_matrix_chain_order(self):
+        n = random.randint(1, 10)
+        dimensions = StandardArray([random.randint(1, 999) for _ in range(n + 1)])
+
+        actual_minimum_costs, optimal_solution = matrix_chain_order(dimensions)
+
+        expected_minimum_cost = get_minimum_matrix_product_cost(dimensions, 1, n)
+        assert_that(actual_minimum_costs[1, n], is_(equal_to(expected_minimum_cost)))
+        expected_minimum_cost = get_matrix_product_cost_based_on_solution(optimal_solution, dimensions, 1, n)
+        assert_that(actual_minimum_costs[1, n], is_(equal_to(expected_minimum_cost)))
+
+    def test_print_optimal_parens(self):
+        n = random.randint(1, 10)
+        s = Matrix.of_dimensions(n, n)
+        for i in between(1, n - 1):
+            for j in between(i + 1, n):
+                s[i, j] = random.randint(i, j - 1)
+        captured_output = io.StringIO()
+
+        with redirect_stdout(captured_output):
+            print_optimal_parens(s, 1, n)
+
+        actual_output = captured_output.getvalue().splitlines()[0]
+        expected_output = get_optimal_parens_brute_force(s, 1, n)
+        assert_that(actual_output, is_(equal_to(expected_output)))
