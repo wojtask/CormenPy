6bdc8a52af84547b1f80b786b540348bb12f50fa
Krzysztof Wojtas <kwojtas@gmail.com>
2017-11-07 12:49:52 +0100

    15.1-4

---
+++ src/chapter15/ex15_1_4.py
@@ -0,0 +1,31 @@
+from datastructures.matrix import Matrix
+from util import between
+
+
+def effective_fastest_way(a, t, e, x, n):
+    f = Matrix.of_dimensions(2, 2)
+    l = Matrix.of_dimensions(2, n)
+    f[1, 2] = e[1] + a[1, 1]
+    f[2, 2] = e[2] + a[2, 1]
+    for j in between(2, n):
+        f[1, 1] = f[1, 2]
+        f[2, 1] = f[2, 2]
+        if f[1, 1] + a[1, j] <= f[2, 1] + t[2, j - 1] + a[1, j]:
+            f[1, 2] = f[1, 1] + a[1, j]
+            l[1, j] = 1
+        else:
+            f[1, 2] = f[2, 1] + t[2, j - 1] + a[1, j]
+            l[1, j] = 2
+        if f[2, 1] + a[2, j] <= f[1, 1] + t[1, j - 1] + a[2, j]:
+            f[2, 2] = f[2, 1] + a[2, j]
+            l[2, j] = 2
+        else:
+            f[2, 2] = f[1, 1] + t[1, j - 1] + a[2, j]
+            l[2, j] = 1
+    if f[1, 2] + x[1] <= f[2, 2] + x[2]:
+        f_star = f[1, 2] + x[1]
+        l_star = 1
+    else:
+        f_star = f[2, 2] + x[2]
+        l_star = 2
+    return f_star, l_star, l
--- test/test_chapter15/test_solutions.py
+++ test/test_chapter15/test_solutions.py
@@ -5,8 +5,10 @@ from unittest import TestCase
 
 from hamcrest import *
 
-from array_util import get_random_matrix
+from array_util import get_random_matrix, get_random_array
 from chapter15.ex15_1_1 import print_stations_
+from chapter15.ex15_1_4 import effective_fastest_way
+from test_chapter15.test_textbook import get_fastest_way_brute_force, get_assembly_time_based_on_lines
 from util import rbetween
 
 
@@ -31,3 +33,17 @@ class Solutions14Test(TestCase):
             expected_output.append('line ' + str(i) + ', station ' + str(j - 1))
         expected_output.reverse()
         assert_that(actual_output, is_(equal_to(expected_output)))
+
+    def test_effective_fastest_way(self):
+        n = random.randint(1, 10)
+        a, _ = get_random_matrix(2, n)
+        t, _ = get_random_matrix(2, n - 1)
+        e, _ = get_random_array(min_size=2, max_size=2)
+        x, _ = get_random_array(min_size=2, max_size=2)
+
+        actual_assembly_time, last_line, lines = effective_fastest_way(a, t, e, x, n)
+
+        expected_assembly_time = get_fastest_way_brute_force(a, t, e, x, n)
+        assert_that(actual_assembly_time, is_(equal_to(expected_assembly_time)))
+        expected_assembly_time = get_assembly_time_based_on_lines(lines, last_line, a, t, e, x, n)
+        assert_that(actual_assembly_time, is_(equal_to(expected_assembly_time)))
