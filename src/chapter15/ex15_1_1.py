664b456316bbc6cfec4f8ae7f0ff54172b6c898d
Krzysztof Wojtas <kwojtas@gmail.com>
2018-01-27 19:56:09 +0100

    changing variable names in print_stations_

--- src/chapter15/ex15_1_1.py
+++ src/chapter15/ex15_1_1.py
@@ -1,4 +1,4 @@
-def print_stations_(l, l_star, n):
-    if n >= 1:
-        print_stations_(l, l[l_star, n], n - 1)
-        print('line ' + str(l_star) + ', station ' + str(n))
+def print_stations_(l, i, j):
+    if j >= 1:
+        print_stations_(l, l[i, j], j - 1)
+        print('line ' + str(i) + ', station ' + str(j))
