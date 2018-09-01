e89127f2cbe1be77494af31346bfffea393857fb
Krzysztof Wojtas <kwojtas@gmail.com>
2018-09-01 22:53:19 +0200

    16.5-2

---
+++ src/chapter16/exercise16_5_2.py
@@ -0,0 +1,16 @@
+from datastructures.array import Array
+from util import between
+
+
+def tasks_independent(A, k):
+    C = Array.indexed(0, k)
+    for i in between(0, k):
+        C[i] = 0
+    for j in between(1, A.length):
+        C[A[j]] = C[A[j]] + 1
+    for i in between(1, k):
+        C[i] = C[i] + C[i - 1]
+    for t in between(1, k):
+        if C[t] > t:
+            return False
+    return True
---
+++ test/test_chapter16/test_exercise16_5_2.py
@@ -0,0 +1,30 @@
+import random
+from unittest import TestCase
+
+from hamcrest import *
+
+from array_util import get_random_array
+from chapter16.exercise16_5_2 import tasks_independent
+from datastructures.array import Array
+from util import between
+
+
+def tasks_independent_bruteforce(deadlines):
+    n = deadlines.length
+    N = Array([len([d for d in deadlines if d <= t]) for t in between(1, n)])
+    for t in between(1, n):
+        if N[t] > t:
+            return False
+    return True
+
+
+class TestExercise16_5_2(TestCase):
+
+    def test_tasks_independent(self):
+        n = random.randint(1, 10)
+        deadlines, _ = get_random_array(max_size=n, min_value=1, max_value=n)
+
+        actual_result = tasks_independent(deadlines, n)
+
+        expected_result = tasks_independent_bruteforce(deadlines)
+        assert_that(actual_result, is_(equal_to(expected_result)))
