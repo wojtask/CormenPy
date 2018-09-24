8c5cce6e4c708034dbb2ced58984f81e6c692bb8
Krzysztof Wojtas <kwojtas@gmail.com>
2018-09-25 00:50:19 +0200

    17-2

---
+++ src/chapter17/problem17_2.py
@@ -0,0 +1,47 @@
+from chapter02.exercise2_3_5 import iterative_binary_search
+from chapter02.textbook2_3 import merge
+from datastructures.array import Array
+from util import between
+
+
+def dynamic_binary_search(A, x):
+    k = A.length
+    for i in between(0, k - 1):
+        if A.length != 0:
+            j = iterative_binary_search(A[i], x)
+            if j is not None:
+                return i, j
+    return None
+
+
+def dynamic_binary_insert(A, x):
+    B = Array([x])
+    i = 0
+    while A[i].length != 0:
+        B = Array(A[i].elements + B.elements)
+        merge(B, 1, A[i].length, B.length)
+        A[i] = Array([])
+        i = i + 1
+    A[i] = Array(B.elements)
+
+
+def dynamic_binary_delete(A, x):
+    i = 0
+    while A[i].length == 0:
+        i = i + 1
+    y = A[i][2 ** i - 1]
+    j = -1
+    l = None
+    while l is None:
+        j = j + 1
+        l = iterative_binary_search(A[j], x)
+    A[j][l] = y
+    while l > 1 and A[j][l] < A[j][l - 1]:
+        A[j][l], A[j][l - 1] = A[j][l - 1], A[j][l]
+    while l < A[j].length and A[j][l] > A[j][l + 1]:
+        A[j][l], A[j][l + 1] = A[j][l + 1], A[j][l]
+    for r in between(0, i - 1):
+        A[r] = Array.indexed(1, 2 ** r)
+        for t in between(1, 2 ** r):
+            A[r][t] = A[i][2 ** r - 2 + t]
+    A[i] = Array([])
---
+++ test/test_chapter17/test_problem17_2.py
@@ -0,0 +1,72 @@
+import random
+from unittest import TestCase
+
+from hamcrest import *
+
+from chapter17.problem17_2 import dynamic_binary_search, dynamic_binary_insert, dynamic_binary_delete
+from datastructures.array import Array
+from util import between
+
+
+def _create_arrays():
+    k = random.randint(1, 5)
+    array = Array.indexed(0, k - 1)
+    for i in between(0, k - 1):
+        if random.randint(0, 1) == 0:
+            array[i] = Array([])
+        else:
+            elements = sorted([random.randint(0, 2 ** (k - 1)) for _ in range(2 ** i)])
+            array[i] = Array(elements)
+    return array, k
+
+
+class TestProblem17_2(TestCase):
+
+    def test_dynamic_binary_search(self):
+        arrays, k = _create_arrays()
+        element = random.randint(0, 2 ** (k - 1))
+
+        p = dynamic_binary_search(arrays, element)
+
+        if p is not None:
+            assert_that(arrays[p[0]][p[1]], is_(equal_to(element)))
+        else:
+            for i in between(0, k - 1):
+                assert_that(element, not_(is_in(arrays[i])))
+
+    def test_dynamic_binary_insert(self):
+        arrays, k = _create_arrays()
+        element = random.randint(0, 2 ** (k - 1))
+        elements_before = [x for arr in arrays for x in arr]
+        # make sure there is a place for the new element
+        if len(elements_before) == 2 ** k - 1:
+            elements_before.remove(arrays[0][1])
+            arrays[0] = Array([])
+
+        dynamic_binary_insert(arrays, element)
+
+        actual_elements_after = [x for arr in arrays for x in arr]
+        expected_elements_after = elements_before + [element]
+        assert_that(actual_elements_after, contains_inanyorder(*expected_elements_after))
+        assert_that(len(actual_elements_after), is_(equal_to(len(expected_elements_after))))
+        for i in between(0, k - 1):
+            assert_that(arrays[i].length, is_(any_of(0, 2 ** i)))
+
+    def test_dynamic_binary_delete(self):
+        arrays, k = _create_arrays()
+        elements_before = [x for arr in arrays for x in arr]
+        # make sure there is an element to remove
+        if len(elements_before) == 0:
+            arrays[0] = Array([random.randint(0, 2 ** (k - 1))])
+            elements_before.append(arrays[0][1])
+        element = random.choice(elements_before)
+
+        dynamic_binary_delete(arrays, element)
+
+        actual_elements_after = [x for arr in arrays for x in arr]
+        expected_elements_after = list(elements_before)
+        expected_elements_after.remove(element)
+        assert_that(actual_elements_after, contains_inanyorder(*expected_elements_after))
+        assert_that(len(actual_elements_after), is_(equal_to(len(expected_elements_after))))
+        for i in between(0, k - 1):
+            assert_that(arrays[i].length, is_(any_of(0, 2 ** i)))
