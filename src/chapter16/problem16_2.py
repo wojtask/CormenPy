e885e1635f124ea4a5551f71714dfa6c9de2aa3e
Krzysztof Wojtas <kwojtas@gmail.com>
2018-10-09 01:44:47 +0200

    16-2(b), renaming average_time_schedule to act_schedule and improving

--- src/chapter16/problem16_2.py
+++ src/chapter16/problem16_2.py
@@ -1,11 +1,60 @@
-from util import between
+import math
+
+from chapter06.exercise6_2_2 import min_heapify
+from chapter06.exercise6_5_3 import min_heap_insert, heap_minimum, heap_extract_min, heap_decrease_key
+from datastructures.array import Array
+from util import between, rbetween
 
 
-def average_time_schedule(P):
-    n = P.length
-    P.elements.sort()
-    sum = c = P[1]
+def act_schedule(p):
+    n = p.length
+    p.elements.sort()
+    c = Array.indexed(1, n)
+    c[1] = p[1]
     for i in between(2, n):
-        c += P[i]
-        sum += c
-    return sum / n
+        c[i] = c[i - 1] + p[i]
+    return c
+
+
+class Activity:
+    def __init__(self, id, processing_time, release_time):
+        self.id = id
+        self.p = processing_time
+        self.r = release_time
+
+    def __lt__(self, other):
+        if isinstance(other, float):
+            return self.p < other
+        return self.p < other.p
+
+    def __gt__(self, other):
+        if isinstance(other, float):
+            return self.p > other
+        return self.p > other.p
+
+
+def _build_min_heap(A):
+    A.heap_size = A.length
+    for i in rbetween(math.floor(A.length / 2), 1):
+        min_heapify(A, i)
+
+
+def preemptive_act_schedule(p, r):
+    n = p.length
+    Q = Array.indexed(1, n)
+    Q.heap_size = 0
+    c = Array.indexed(1, n)
+    for i in between(1, n):
+        t = r[i]
+        min_heap_insert(Q, Activity(i, p[i], r[i]))
+        while t < r[i + 1] and Q.heap_size != 0:
+            a = heap_minimum(Q)
+            if t + a.p <= r[i + 1]:
+                heap_extract_min(Q)
+                t = t + a.p
+                c[a.id] = t
+            else:
+                a_ = Activity(a.id, a.p - (r[i + 1] - t), a.r)
+                heap_decrease_key(Q, 1, a_)
+                t = r[i + 1]
+    return c
--- test/test_chapter16/test_problem16_2.py
+++ test/test_chapter16/test_problem16_2.py
@@ -1,34 +1,97 @@
+import math
+import random
 from unittest import TestCase
 
 from hamcrest import *
 
 from array_util import get_random_array
-from chapter16.problem16_2 import average_time_schedule
+from chapter16.problem16_2 import act_schedule, preemptive_act_schedule, Activity
 from datastructures.array import Array
+from util import between
 
 
-def get_min_average_completion_time_bruteforce(processing_times):
-    return compute_average_completion_time(Array(sorted(processing_times)))
+def get_min_act_bruteforce(processing_times):
+    return compute_act(Array(sorted(processing_times)))
 
 
-def compute_average_completion_time(schedule):
-    average_completion_time = 0
+def compute_act(schedule):
+    act = 0
     completion_time = 0
     for processing_time in schedule:
         completion_time += processing_time
-        average_completion_time += completion_time
-    return average_completion_time / schedule.length
+        act += completion_time
+    return act / schedule.length
+
+
+def sort_activities_by_release_time(p, r):
+    sorted_tuples = sorted(zip(p, r), key=lambda x: x[1])
+    p.elements = [x[0] for x in sorted_tuples]
+    r.elements = [x[1] for x in sorted_tuples]
+
+
+def get_min_schedule(activities, schedule, time):
+    min_schedule_cost = math.inf
+    min_schedule = Array(schedule.elements)
+    earliest_future_release_time = math.inf
+    for i in range(len(activities)):
+        activity = activities[i]
+        if activity.r <= time and activity.p > 0:
+            activity.p -= 1
+            deleted = False
+            if activity.p == 0:
+                schedule[activity.id] = time + 1
+                del activities[i]
+                deleted = True
+            schedule = get_min_schedule(activities, schedule, time + 1)
+            cost = sum(schedule.elements)
+            if cost < min_schedule_cost:
+                min_schedule_cost = cost
+                min_schedule = Array(schedule.elements)
+            if deleted:
+                activities.insert(i, activity)
+            activity.p += 1
+        if activity.r > time and activity.p > 0:
+            earliest_future_release_time = min(earliest_future_release_time, activity.r)
+    if earliest_future_release_time < math.inf:
+        schedule = get_min_schedule(activities, schedule, earliest_future_release_time)
+        if sum(schedule.elements) < min_schedule_cost:
+            return Array(schedule.elements)
+    return min_schedule
+
+
+def get_min_preemptive_act_bruteforce(processing_times, release_times):
+    n = processing_times.length
+    activities = [Activity(i, processing_times[i], release_times[i]) for i in between(1, n)]
+    schedule = Array([None] * n)
+    return sum(get_min_schedule(activities, schedule, 0)) / n
 
 
 class TestProblem16_2(TestCase):
 
-    def test_average_time_schedule(self):
+    def test_act_schedule(self):
         processing_times, processing_times_elements = get_random_array()
         original_processing_times = Array(processing_times_elements)
 
-        actual_average_completion_time = average_time_schedule(processing_times)
+        actual_schedule = act_schedule(processing_times)
 
-        expected_min_average_completion_time = get_min_average_completion_time_bruteforce(original_processing_times)
-        assert_that(actual_average_completion_time, is_(equal_to(expected_min_average_completion_time)))
-        average_completion_time_from_schedule = compute_average_completion_time(processing_times)
-        assert_that(average_completion_time_from_schedule, is_(equal_to(expected_min_average_completion_time)))
+        actual_min_act = sum(actual_schedule.elements) / actual_schedule.length
+        expected_min_act = get_min_act_bruteforce(original_processing_times)
+        assert_that(actual_min_act, is_(equal_to(expected_min_act)))
+
+    def test_preemptive_act_schedule(self):
+        n = random.randint(1, 4)
+        processing_times, processing_times_elements = get_random_array(min_size=n, max_size=n, min_value=1, max_value=3)
+        release_times, release_times_elements = get_random_array(min_size=n, max_size=n, max_value=15)
+        original_processing_times = Array(processing_times_elements)
+        original_release_times = Array(release_times_elements)
+        sort_activities_by_release_time(processing_times, release_times)
+        release_times.elements.append(math.inf)
+        release_times.length += 1
+
+        actual_schedule = preemptive_act_schedule(processing_times, release_times)
+
+        for i in between(1, n):
+            assert_that(actual_schedule[i], is_(greater_than(release_times[i])))
+        actual_min_act = sum(actual_schedule.elements) / actual_schedule.length
+        expected_min_act = get_min_preemptive_act_bruteforce(original_processing_times, original_release_times)
+        assert_that(actual_min_act, is_(equal_to(expected_min_act)))
