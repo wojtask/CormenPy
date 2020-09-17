42b0cca3da2c81052ad0d818e0b0ef320829ef84
Krzysztof Wojtas <kwojtas@gmail.com>
2020-09-17 18:26:32 +0200

    18.2 -- b_tree_create

--- src/chapter18/textbook18_2.py
+++ src/chapter18/textbook18_2.py
@@ -1,4 +1,4 @@
-from datastructures.b_tree import disk_read
+from datastructures.b_tree import disk_read, allocate_node, disk_write
 
 
 def b_tree_search(x, k):
@@ -12,3 +12,11 @@ def b_tree_search(x, k):
     else:
         disk_read(x.c[i])
         return b_tree_search(x.c[i], k)
+
+
+def b_tree_create(T):
+    x = allocate_node()
+    x.leaf = True
+    x.n = 0
+    disk_write(x)
+    T.root = x
