c536ffba5f6a8b667d13a17a5892af671d38b027
Krzysztof Wojtas <kwojtas@gmail.com>
2020-09-18 01:51:21 +0200

    18.2-6 -- b_tree_binary_search

---
+++ src/chapter18/textbook_exercise18_2_6.py
@@ -0,0 +1,21 @@
+import math
+
+from datastructures.b_tree import disk_read
+
+
+def b_tree_binary_search(x, k):
+    low = 1
+    high = x.n
+    while low <= high:
+        mid = math.floor((low + high) / 2)
+        if k == x.key[mid]:
+            return x, mid
+        if k < x.key[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    if x.leaf:
+        return None
+    else:
+        disk_read(x.c[low])
+        return b_tree_binary_search(x.c[low], k)
