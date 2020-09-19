d761ccb2e8645a137d18e6f7f0a37237a4464940
Krzysztof Wojtas <kwojtas@gmail.com>
2020-09-19 23:44:54 +0200

    18.2-3 -- b_tree_minimum, b_tree_maximum, b_tree_predecessor

---
+++ src/chapter18/exercise18_2_3.py
@@ -0,0 +1,35 @@
+from datastructures.b_tree import disk_read
+
+
+def b_tree_minimum(x):
+    while not x.leaf:
+        disk_read(x.c[1])
+        x = x.c[1]
+    return x.key[1]
+
+
+def b_tree_maximum(x):
+    while not x.leaf:
+        disk_read(x.c[x.n + 1])
+        x = x.c[x.n + 1]
+    return x.key[x.n]
+
+
+def b_tree_predecessor(T, x, i):
+    if not x.leaf:
+        disk_read(x.c[i])
+        return b_tree_maximum(x.c[i])
+    if i > 1:
+        return x.key[i - 1]
+    k = x.key[i]
+    l = None
+    y = T.root
+    while y != x:
+        j = 1
+        while j <= y.n and k > y.key[j]:
+            j += 1
+        if j > 1:
+            l = y.key[j - 1]
+        disk_read(y.c[j])
+        y = y.c[j]
+    return l
