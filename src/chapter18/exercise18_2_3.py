cb3ffc19ccd49881cdfb5668740bbbbd0983709d
Krzysztof Wojtas <kwojtas@gmail.com>
2021-07-15 15:59:16 +0200

    18.2-3 -- change variable name in b_tree_predecessor

--- src/chapter18/exercise18_2_3.py
+++ src/chapter18/exercise18_2_3.py
@@ -22,14 +22,14 @@ def b_tree_predecessor(T, x, i):
     if i > 1:
         return x.key[i - 1]
     k = x.key[i]
-    l = None
+    k_ = None
     y = T.root
     while y != x:
         j = 1
         while j <= y.n and k > y.key[j]:
             j += 1
         if j > 1:
-            l = y.key[j - 1]
+            k_ = y.key[j - 1]
         disk_read(y.c[j])
         y = y.c[j]
-    return l
+    return k_
