1b18aae96307138438ddc97e12839e87065fb624
Krzysztof Wojtas <kwojtas@gmail.com>
2021-07-15 16:00:37 +0200

    added GuardedArray to ensure that writing to key and c attributes requires saving the node on disk, added free_node

--- src/datastructures/b_tree.py
+++ src/datastructures/b_tree.py
@@ -17,9 +17,22 @@ class Node:
 
     def __setattr__(self, name, value):
         if id(self) not in in_memory_node_ids:
-            raise AttributeError("Attempted to change an attribute of a node before reading it from disk")
+            raise AttributeError("Attempted to write an attribute of a node before reading it from disk")
         unsaved_node_ids.add(id(self))
         super().__setattr__(name, value)
+
+
+class GuardedArray(Array):
+    """Meant to use as a type of key and c attributes in BTree nodes. Ensures that setting an item in its instance will
+    require the owning node to be saved on disk."""
+
+    def __init__(self, elements, node_id):
+        super(GuardedArray, self).__init__(elements, start=1)
+        self.node_id = node_id
+
+    def __setitem__(self, index, item):
+        unsaved_node_ids.add(self.node_id)
+        super().__setitem__(index, item)
 
 
 def disk_read(x):
@@ -33,6 +46,15 @@ def disk_write(x):
 def allocate_node(t=2):
     x = Node()
     in_memory_node_ids.add(id(x))
-    x.key = Array.indexed(1, 2 * t - 1)
-    x.c = Array.indexed(1, 2 * t)
+    x.key = GuardedArray([None] * (2 * t - 1), id(x))
+    x.c = GuardedArray([None] * (2 * t), id(x))
     return x
+
+
+def free_node(x):
+    try:
+        in_memory_node_ids.remove(id(x))
+        unsaved_node_ids.remove(id(x))
+    except KeyError:
+        pass
+    del x
