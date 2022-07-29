abea1f42af26f2512c57d9bd90bb0b3cd5a5a310
Krzysztof Wojtas <kwojtas@gmail.com>
2022-07-29 21:55:48 +0200

    9.3-8 -- small improvement in the algorithm, renaming it to joint_median

--- test/test_chapter09/test_exercise9_3_8.py
+++ test/test_chapter09/test_exercise9_3_8.py
@@ -4,17 +4,17 @@ from unittest import TestCase
 from hamcrest import *
 
 from array_util import get_random_array
-from chapter09.exercise9_3_8 import two_arrays_median
+from chapter09.exercise9_3_8 import joint_median
 
 
 class TestExercise9_3_8(TestCase):
 
-    def test_two_arrays_median(self):
+    def test_joint_median(self):
         n = random.randint(1, 20)
         array1 = get_random_array(size=n).sort().save_state()
         array2 = get_random_array(size=n).sort().save_state()
 
-        actual_median = two_arrays_median(array1, 1, n, array2, 1, n)
+        actual_median = joint_median(array1, 1, n, array2, 1, n)
 
         assert_that(array1.is_modified(), is_(False))
         assert_that(array2.is_modified(), is_(False))
