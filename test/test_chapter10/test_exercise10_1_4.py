fbbf2489b23e8ed6f10bdc874cea5e0997873aa7
Krzysztof Wojtas <kwojtas@gmail.com>
2022-11-06 00:42:25 +0100

    10.1-4 -- extracting a new function queue_full, reorganizing code

--- test/test_chapter10/test_exercise10_1_4.py
+++ test/test_chapter10/test_exercise10_1_4.py
@@ -4,12 +4,29 @@ from unittest import TestCase
 from hamcrest import *
 
 from array_util import get_random_array
-from chapter10.exercise10_1_4 import queue_empty, enqueue_, dequeue_
+from chapter10.exercise10_1_4 import dequeue_, queue_empty, enqueue_, queue_full
 from datastructures.array import Array
 from datastructures.queue import Queue
 
 
 class TestExercise10_1_4(TestCase):
+
+    def test_dequeue_(self):
+        size = 5
+        queue = Queue(get_random_array(size=size), head=random.randint(1, size), tail=random.randint(1, size))
+
+        if queue.head == queue.tail:
+            assert_that(calling(dequeue_).with_args(queue), raises(ValueError, 'underflow'))
+        else:
+            expected_elements = Array(queue)
+            expected_elements.pop(1)
+            expected_deleted = queue[queue.head]
+
+            actual_deleted = dequeue_(queue)
+
+            assert_that(actual_deleted, is_(equal_to(expected_deleted)))
+            actual_elements = Array(queue)
+            assert_that(actual_elements, is_(equal_to(expected_elements)))
 
     def test_queue_empty(self):
         size = 5
@@ -38,19 +55,16 @@ class TestExercise10_1_4(TestCase):
             actual_elements = Array(queue)
             assert_that(actual_elements, is_(equal_to(expected_elements)))
 
-    def test_dequeue_(self):
+    def test_queue_full(self):
         size = 5
         queue = Queue(get_random_array(size=size), head=random.randint(1, size), tail=random.randint(1, size))
 
-        if queue.head == queue.tail:
-            assert_that(calling(dequeue_).with_args(queue), raises(ValueError, 'underflow'))
+        actual_full = queue_full(queue)
+
+        assert_that(queue.is_modified(), is_(False))
+        if queue.head == queue.tail + 1:
+            assert_that(actual_full, is_(True))
+        elif queue.head == 1 and queue.tail == queue.length:
+            assert_that(actual_full, is_(True))
         else:
-            expected_elements = Array(queue)
-            expected_elements.pop(1)
-            expected_deleted = queue[queue.head]
-
-            actual_deleted = dequeue_(queue)
-
-            assert_that(actual_deleted, is_(equal_to(expected_deleted)))
-            actual_elements = Array(queue)
-            assert_that(actual_elements, is_(equal_to(expected_elements)))
+            assert_that(actual_full, is_(False))
