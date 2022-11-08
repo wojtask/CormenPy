eeea0709180269e6d0abfed8ce05b22e6cca3dfe
Krzysztof Wojtas <kwojtas@gmail.com>
2022-11-08 23:49:45 +0100

    renaming procedures recursive_tree_insert, recursive_tree_insert_wrapper

--- src/chapter12/exercise12_3_1.py
+++ src/chapter12/exercise12_3_1.py
@@ -1,14 +1,14 @@
-def recursive_tree_insert(x, z):
+def recursive_subtree_insert(x, z):
     if x is None:
         return z
     if z.key < x.key:
-        x.left = recursive_tree_insert(x.left, z)
+        x.left = recursive_subtree_insert(x.left, z)
         x.left.p = x
     else:
-        x.right = recursive_tree_insert(x.right, z)
+        x.right = recursive_subtree_insert(x.right, z)
         x.right.p = x
     return x
 
 
-def recursive_tree_insert_wrapper(T, z):
-    T.root = recursive_tree_insert(T.root, z)
+def recursive_tree_insert(T, z):
+    T.root = recursive_subtree_insert(T.root, z)
