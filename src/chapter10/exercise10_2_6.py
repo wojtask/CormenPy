17f6ac5bb22770d3a43c4b4d5a7f313819564147
Krzysztof Wojtas <kwojtas@gmail.com>
2022-11-08 03:59:56 +0100

    simplifying circular_lists_union by using doubly linked lists with sentinels, renaming function to list_union

--- src/chapter10/exercise10_2_6.py
+++ src/chapter10/exercise10_2_6.py
@@ -1,15 +1,6 @@
-from datastructures.list import List
-
-
-def circular_lists_union(S1, S2):
-    S = List()
-    if S1.head is not None and S2.head is not None:
-        x = S1.head.next
-        S1.head.next = S2.head.next
-        S2.head.next = x
-    if S1.head is not None:
-        S.head = S1.head
-    else:
-        S.head = S2.head
-    S1.head = S2.head = None
-    return S
+def list_union(L1, L2):
+    L2.nil.next.prev = L1.nil.prev
+    L1.nil.prev.next = L2.nil.next
+    L2.nil.prev.next = L1.nil
+    L1.nil.prev = L2.nil.prev
+    return L1
--- test/test_chapter10/test_exercise10_2_6.py
+++ test/test_chapter10/test_exercise10_2_6.py
@@ -2,19 +2,19 @@ from unittest import TestCase
 
 from hamcrest import *
 
-from chapter10.exercise10_2_6 import circular_lists_union
-from list_util import get_random_circular_singly_linked_list
+from chapter10.exercise10_2_6 import list_union
+from list_util import get_random_doubly_linked_list_with_sentinel
 
 
 class TestExercise10_2_6(TestCase):
 
-    def test_circular_lists_union(self):
-        linked_list1 = get_random_circular_singly_linked_list()
+    def test_list_union(self):
+        linked_list1 = get_random_doubly_linked_list_with_sentinel(min_size=0)
         original_keys1 = linked_list1.as_keys_array()
-        linked_list2 = get_random_circular_singly_linked_list()
+        linked_list2 = get_random_doubly_linked_list_with_sentinel(min_size=0)
         original_keys2 = linked_list2.as_keys_array()
 
-        actual_union = circular_lists_union(linked_list1, linked_list2)
+        actual_union = list_union(linked_list1, linked_list2)
 
         actual_keys = actual_union.as_keys_array()
         expected_keys = original_keys1 + original_keys2
