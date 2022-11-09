56a9ec1e9e4afcd99e51b3e32857e4d9c98af747
Krzysztof Wojtas <kwojtas@gmail.com>
2022-11-10 00:57:36 +0100

    remove the parent pointer from AVLNode, refactor and rename AVL tree procedures

--- test/test_chapter13/test_problem13_3.py
+++ test/test_chapter13/test_problem13_3.py
@@ -3,9 +3,9 @@ from unittest import TestCase
 from hamcrest import *
 
 from array_util import get_random_array
-from chapter13.problem13_3 import avl_insert_wrapper
+from chapter13.problem13_3 import avl_insert
 from datastructures.binary_tree import AVLNode, BinaryTree
-from tree_util import assert_avl_tree, assert_parent_pointers_consistent, get_binary_search_tree_inorder_keys
+from tree_util import assert_avl_tree, get_binary_search_tree_inorder_keys
 
 
 class TestProblem13_3(TestCase):
@@ -15,10 +15,9 @@ class TestProblem13_3(TestCase):
         tree = BinaryTree()
 
         for key in keys:
-            avl_insert_wrapper(tree, AVLNode(key))
+            avl_insert(tree, AVLNode(key))
 
             assert_avl_tree(tree)
-            assert_parent_pointers_consistent(tree)
 
         actual_keys = get_binary_search_tree_inorder_keys(tree)
         assert_that(actual_keys, contains_inanyorder(*keys))
