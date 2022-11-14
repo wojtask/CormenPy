1fc1be89a7141ce20a12530b216705bd718684d3
Krzysztof Wojtas <kwojtas@gmail.com>
2022-11-14 23:29:47 +0100

    simplify knapsack, rename the main array

--- src/chapter16/exercise16_2_2.py
+++ src/chapter16/exercise16_2_2.py
@@ -4,21 +4,22 @@ from util import between
 
 def knapsack(w, v, W):
     n = w.length
-    K = Array((Array.indexed(0, W) for _ in between(0, n)), start=0)
+    V = Array((Array.indexed(0, W) for _ in between(0, n)), start=0)
     for j in between(0, W):
-        K[0, j] = 0
+        V[0, j] = 0
     for i in between(1, n):
         for j in between(0, W):
-            K[i, j] = K[i - 1, j]
-            if w[i] <= j and K[i - 1, j - w[i]] + v[i] > K[i, j]:
-                K[i, j] = K[i - 1, j - w[i]] + v[i]
-    return K
+            if w[i] > j:
+                V[i, j] = V[i - 1, j]
+            else:
+                V[i, j] = max(V[i - 1, j], V[i - 1, j - w[i]] + v[i])
+    return V
 
 
-def print_knapsack(K, w, i, j):
+def print_knapsack(V, w, i, j):
     if i >= 1:
-        if K[i, j] == K[i - 1, j]:
-            print_knapsack(K, w, i - 1, j)
+        if V[i, j] == V[i - 1, j]:
+            print_knapsack(V, w, i - 1, j)
         else:
-            print_knapsack(K, w, i - 1, j - w[i])
+            print_knapsack(V, w, i - 1, j - w[i])
             print('a' + str(i))
