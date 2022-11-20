c4a5f0974e5c1e69dbb83af18c090f664ed40962
Krzysztof Wojtas <kwojtas@gmail.com>
2022-11-20 01:52:43 +0100

    simplifying compactify_list

--- src/chapter10/exercise10_3_5.py
+++ src/chapter10/exercise10_3_5.py
@@ -1,52 +1,26 @@
 def compactify_list(L):
-    m = _mark_prev_fields(L)
-    x = L.head
-    x_ = None
-    y = 1
-    while x is not None:
-        if x <= m:
-            x_ = x
-            x = L.next[x]
-        else:
-            while L.prev[y] == -1:
-                y += 1
-            _swap_all_fields(L, x, y)
-            if L.next[x] is not None:
-                L.prev[L.next[x]] = x
-            if L.prev[x] is not None:
-                L.next[L.prev[x]] = x
-            else:
-                L.free = x
-            if x_ is not None:
-                L.next[x_] = y
-            else:
-                L.head = y
-            x_ = y
-            x = L.next[y]
-    _restore_prev_fields(L)
+    k = 1
+    l = L.head
+    while l is not None:
+        _swap_elements(L, k, l)
+        if L.free == k:
+            L.free = l
+        l = L.next[k]
+        L.next[k] = k + 1
+        L.prev[k] = k - 1
+        k += 1
+    L.prev[1] = L.next[k - 1] = None
+    L.head = 1
 
 
-def _mark_prev_fields(L):
-    size = 0
-    x = L.head
-    while x is not None:
-        L.prev[x] = -1
-        size += 1
-        x = L.next[x]
-    return size
-
-
-def _swap_all_fields(L, x, y):
-    L.key[x], L.key[y] = L.key[y], L.key[x]
+def _swap_elements(L, x, y):
+    z = L.next[y]
+    if L.prev[x] is not None:
+        L.next[L.prev[x]] = y
+    if L.next[x] is not None:
+        L.prev[L.next[x]] = y
+    if z is not None:
+        L.prev[z] = x
     L.next[x], L.next[y] = L.next[y], L.next[x]
     L.prev[x], L.prev[y] = L.prev[y], L.prev[x]
-
-
-def _restore_prev_fields(L):
-    if L.head is None:
-        return
-    L.prev[L.head] = None
-    x = L.head
-    while L.next[x] is not None:
-        L.prev[L.next[x]] = x
-        x = L.next[x]
+    L.key[x], L.key[y] = L.key[y], L.key[x]
