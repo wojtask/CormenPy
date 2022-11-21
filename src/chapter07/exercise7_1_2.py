2641848e842f34d3e0e53231f91a7f7494840f92
Krzysztof Wojtas <kwojtas@gmail.com>
2022-11-21 12:12:40 +0100

    generalize fair_partition

--- src/chapter07/exercise7_1_2.py
+++ src/chapter07/exercise7_1_2.py
@@ -4,14 +4,12 @@ from util import between
 def fair_partition(A, p, r):
     x = A[r]
     i = p - 1
-    equal = 0
+    d = 1
     for j in between(p, r - 1):
         if A[j] <= x:
             if A[j] == x:
-                equal += 1
+                d += 1
             i += 1
             A[i], A[j] = A[j], A[i]
-    if equal == r - p:
-        return (p + r) // 2
     A[i + 1], A[r] = A[r], A[i + 1]
-    return i + 1
+    return i + 1 - d // 2
