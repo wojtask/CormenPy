9fa8050e4b3294399694137238601297db50a8a2
Krzysztof Wojtas <kwojtas@gmail.com>
2022-11-26 17:48:54 +0100

    minor refactor of delete_larger_half

--- src/chapter17/exercise17_3_7.py
+++ src/chapter17/exercise17_3_7.py
@@ -1,6 +1,7 @@
 from chapter09.textbook9_3 import select
 from chapter10.textbook10_2 import list_insert, list_delete
 from datastructures.array import Array
+from util import ceildiv
 
 
 def _transform_to_array(S):
@@ -19,16 +20,16 @@ def dlh_insert(S, x):
 def delete_larger_half(S):
     A = _transform_to_array(S)
     M = select(A, 1, A.length, (A.length + 1) // 2)
-    size = A.length
+    deleted = 0
     x = S.head
     while x is not None:
         if x.key > M:
             list_delete(S, x)
-            size -= 1
+            deleted += 1
         x = x.next
     x = S.head
-    while size > A.length // 2:
+    while deleted < ceildiv(A.length, 2):
         if x.key == M:
             list_delete(S, x)
-            size -= 1
+            deleted += 1
         x = x.next
