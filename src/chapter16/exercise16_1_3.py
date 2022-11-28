44357d387ecdfa157dde9465e881fca35aaa6d04
Krzysztof Wojtas <kwojtas@gmail.com>
2022-11-28 03:10:08 +0100

    simplify activity_scheduler

--- src/chapter16/exercise16_1_3.py
+++ src/chapter16/exercise16_1_3.py
@@ -1,30 +1,23 @@
 from chapter10.textbook10_1 import pop, push
-from chapter13.textbook13_2 import rb_search
-from chapter13.textbook13_3 import rb_insert
-from chapter13.textbook13_4 import rb_delete
 from datastructures.array import Array
-from datastructures.red_black_tree import RedBlackTree, Node
+from datastructures.stack import Stack
 from util import rbetween
 
 
 def activity_scheduler(s, f):
     n = s.length
     A = Array.indexed(1, n)
-    F = Array(rbetween(n, 1))
-    F.top = n
-    B = RedBlackTree()
+    F = Stack(rbetween(n, 1), top=n)
+    H = Array.indexed(1, n)
     # events contains triples (a, b, c) where a = 0 if the event is finish of an activity and 1 if it is start,
-    # b as the activity number, and c as the start time or the finish time
+    # b as the activity number, and c is the start time or the finish time
     events = Array((0, i + 1, finish_time) for i, finish_time in enumerate(f)) + \
         Array((1, i + 1, start_time) for i, start_time in enumerate(s))
     events.sort(key=lambda e: (e[2], e[0]))
     for e in events:
         if e[0] == 1:
-            hall_number = pop(F)
-            A[e[1]] = hall_number
-            rb_insert(B, Node(e[1], data=hall_number), sentinel=B.nil)
+            A[e[1]] = H[e[1]] = pop(F)
         else:
-            hall = rb_search(B.root, e[1], sentinel=B.nil)
-            push(F, hall.data)
-            rb_delete(B, hall, sentinel=B.nil)
+            push(F, H[e[1]])
+            H[e[1]] = None
     return A
